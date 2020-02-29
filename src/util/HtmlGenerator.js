2fac10f7bc0205a9f26046f65dcb4480bae4b578
Mac <mac@Macs-Air.fritz.box>
2020-02-29 11:45:51 +0100

    retrieve more images HtmlGenerator

--- src/util/HtmlGenerator.js
+++ src/util/HtmlGenerator.js
@@ -3,7 +3,10 @@ const createHtmlFromItem = (item) => {
         <div>
         <b>${item.price.value} ${item.price.currency}</b>
         </div>
-        <img width=500 src='${item.image.imageUrl}'/>`
+        <img width=500 src='${item.image.imageUrl}'/>
+        <img width=500 src='${item.additionalImages[0].imageUrl}'/>
+        <img width=500 src='${item.additionalImages[1].imageUrl}'/>
+        <img width=500 src='${item.additionalImages[2].imageUrl}'/>`
 }
 
 module.exports = { createHtmlFromItem }
