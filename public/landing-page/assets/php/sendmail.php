d2d6f6a09e1ac82de139bc52399da67a508c61fe
Joyce <joyce_marvin.rafflenbeul@fh-bielefeld.de>
2020-04-15 17:00:39 +0200

    removed js from landing page

--- public/landing-page/assets/php/sendmail.php
+++ public/landing-page/assets/php/sendmail.php
@@ -7,4 +7,5 @@ $message = 'Name: '.$name.'<br/>'.'E-Mail: '.$email.'<br/>'.'Nachricht: '.$msg;
 $message = wordwrap($msg,70);
 
 mail("admin@dem-it.de","Kontaktformular auction-template.com", $message);
+
 ?>
