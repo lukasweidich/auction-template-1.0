6ade85f7cd7301007de316d2aa2887f1a1a203ed
Mac <mac@Macs-Air.fritz.box>
2020-04-20 17:36:45 +0200

    desgin fixes correctur

--- src/screens/HowToScreen.js
+++ src/screens/HowToScreen.js
@@ -3,16 +3,17 @@ import StyledPage from "../components/StyledPage"
 import WalkthroughComponent from "../components/WalkthroughComponent"
 import WalkthroughComponentGroup from "../components/WalkthroughComponentGroup"
 import "../App.css"
+import "./HowToScreen.css"
 
 const HowToScreen = props => {
     return (
         <StyledPage style={{ backgroundColor: "#e6e6e6", minHeight: "100vh" }}>
-            <WalkthroughComponentGroup style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+            <WalkthroughComponentGroup class="auction-template-howto-component-group">
                 <WalkthroughComponent
                     title={"Einen Artikel einladen"}
                     subheader={"Ohne Artikelnummer"}
                     src={"https://dem-it.de/auction-template/walkthrough/seller_filled.png"}
-                    text={"Geben Sie Ihren eBay Nutzernamen ein, um Ihre inserierten Produkte zu laden. Aus diesen können Sie nun den gewünschten Artikel auswählen. Drücken Sie auf den Knopf, um die Auktionsvorlage automatisch generieren zu lassen."}
+                    text={"Geben Sie Ihren eBay-Nutzernamen ein, um Ihre inserierten Produkte zu laden. Aus diesen können Sie nun den gewünschten Artikel auswählen. Drücken Sie auf den Knopf 'PRODUKTBESCHREIBUNG GENERIEREN', um die Auktionsvorlage automatisch generieren zu lassen."}
                     ratio={"70%"}
                     maxWidth={"500px"}
                 />
@@ -20,12 +21,12 @@ const HowToScreen = props => {
                     title={"Einen Artikel einladen"}
                     subheader={"Mit Artikelnummer"}
                     src={"https://dem-it.de/auction-template/walkthrough/itemid_filled.png"}
-                    text={"Geben Sie die eBay Artikelnummer des gewünschten Inserats ein. Drücken Sie auf den Knopf, um die Auktionsvorlage automatisch generieren zu lassen."}
+                    text={"Geben Sie die eBay-Artikelnummer des gewünschten Inserats ein. Drücken Sie auf den Knopf 'PRODUKTBESCHREIBUNG GENERIEREN', um die Auktionsvorlage automatisch generieren zu lassen."}
                     ratio={"70%"}
                     maxWidth={"500px"}
                 />
             </WalkthroughComponentGroup>
-            <WalkthroughComponentGroup style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <WalkthroughComponentGroup class="auction-template-howto-component-group-single">
                 <WalkthroughComponent
                     title={"Artikelinformationen bearbeiten"}
                     src={"https://dem-it.de/auction-template/walkthrough/generalInformation.png"}
@@ -34,12 +35,12 @@ const HowToScreen = props => {
                     maxWidth={"1200px"}
                 />
             </WalkthroughComponentGroup>
-            <WalkthroughComponentGroup style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+            <WalkthroughComponentGroup class="auction-template-howto-component-group">
                 <WalkthroughComponent
                     title={"Eigenschaften bearbeiten"}
                     subheader={"Kürzen, Ergänzen"}
                     src={"https://dem-it.de/auction-template/walkthrough/aspects1.png"}
-                    text={"Die Artikeleigenschaften stammen aus dem eBay Inserat. Sollten diese jedoch nicht Ihrer Vorstellung entsprechen, können Sie die Eigenschaften einfach an Ihre Vorstellungen anpassen."}
+                    text={"Die Artikeleigenschaften stammen aus dem geladenen eBay-Inserat. Sollte die Anordnung oder Beschriftungen von einzelnen Elementen der Artikelmerkmale jedoch nicht Ihrer Vorstellung entsprechen, können Sie die Eigenschaften einfach an Ihre Vorstellungen anpassen."}
                     ratio={"70%"}
                     maxWidth={"500px"}
                 />
@@ -47,17 +48,17 @@ const HowToScreen = props => {
                     title={"Eigenschaften bearbeiten"}
                     subheader={"Verschieben, Hinzufügen, Entfernen"}
                     src={"https://dem-it.de/auction-template/walkthrough/aspects2.png"}
-                    text={"Merkmalgruppen können nach Belieben hinzugefügt und entfernt werden. In ihnen befinden sich Merkmale, die ebenfalls flexibel an Position und Inhalt anpassen."}
+                    text={"Merkmalgruppen können nach Belieben hinzugefügt und entfernt werden. In diesen befinden sich Merkmale, die ebenfalls flexibel an Position und Inhalt anpassbar sind."}
                     ratio={"70%"}
                     maxWidth={"500px"}
                 />
             </WalkthroughComponentGroup>
-            <WalkthroughComponentGroup style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <WalkthroughComponentGroup class="auction-template-howto-component-group-single">
                 <WalkthroughComponent
                     title={"Dienstleistungen"}
                     subheader={"Bezahl- und Versandmethoden"}
                     src={"https://dem-it.de/auction-template/walkthrough/payment-shipping.png"}
-                    text={"Auch dieser Abschnitt wird durch die Angaben im eBay Inserat befüllt. Sie können die Bezahl- und Versandmethoden problemlos anpassen."}
+                    text={"Auch dieser Abschnitt wird durch die Angaben im eBay-Inserat befüllt. Sie können die Bezahl- und Versandmethoden problemlos anpassen."}
                     ratio={"70%"}
                     maxWidth={"500px"}
                 />
@@ -65,7 +66,7 @@ const HowToScreen = props => {
                     title={"Ergänzende Texte"}
                     subheader={"Um Ihre Auktionsvorlage zu vervollständigen"}
                     src={"https://dem-it.de/auction-template/walkthrough/additionalTexts.png"}
-                    text={"Fügen Sie nach Belieben weitere Textabschnitte in Ihre Auktionsvorlage ein."}
+                    text={"Fügen Sie nach Belieben weitere Textabschnitte wie z.B. 'Rechtliche Informationen' oder weitere Anmerkungen in Ihre Auktionsvorlage ein."}
                     ratio={"31.25%"}
                     maxWidth={"1000px"}
                 />
@@ -73,5 +74,5 @@ const HowToScreen = props => {
         </StyledPage >
     )
 }
-// 
+//
 export default HowToScreen;
--- src/screens/Imprint.js
+++ src/screens/Imprint.js
@@ -1,10 +1,85 @@
 import React from "react"
 import StyledPage from "../components/StyledPage"
+import "./Imprint.css"
 
 const Imprint = props => {
     return (
-        <StyledPage style={{ height: "100vh", backgroundColor: "#e6e6e6" }}>
+        <StyledPage class="auction-template-imprint-container">
+          <div class="auction-template-imprint-wrapper">
             <h1>Impressum</h1>
+            <div class="auction-template-imprint-legal-info-wrapper">
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-left">
+                  <h4>Herausgeber</h4>
+                </div>
+              </div>
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-right">
+                  <h4>Auction Template</h4>
+                  <h4>Hahler Straße 285</h4>
+                  <h4>32427 Minden</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="auction-template-imprint-legal-info-wrapper">
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-left">
+                  <h4>Vertreten durch</h4>
+                </div>
+              </div>
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-right">
+                  <h4>demIT</h4>
+                  <h4>Inh. Joyce Marvin Rafflenbeul</h4>
+                  <h4>Hahler Straße 285</h4>
+                  <h4>32427 Minden</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="auction-template-imprint-legal-info-wrapper">
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-left">
+                  <h4>Kontakt</h4>
+                </div>
+              </div>
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-right">
+                  <h4>info@dem-it.de</h4>
+                  <h4>www.dem-it.de</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="auction-template-imprint-legal-info-wrapper">
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-left">
+                  <h4>Steuernummer</h4>
+                </div>
+              </div>
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-right">
+                  <h4>335 / 5181 / 2442</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="auction-template-imprint-legal-info-wrapper">
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-left">
+                  <h4>Finanzamt</h4>
+                </div>
+              </div>
+              <div class="auction-template-imprint-legal-info-50">
+                <div cass="auction-template-imprint-legal-info-right">
+                  <h4>Minden</h4>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
         </StyledPage>
     )
 }
