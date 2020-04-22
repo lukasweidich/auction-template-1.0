cb5bc89e89b5572cd82f2d0494d51efabac5e707
Mac <mac@Macs-Air.fritz.box>
2020-04-22 14:37:46 +0200

    sitemap + agb

--- src/screens/TermsAndConditions.js
+++ src/screens/TermsAndConditions.js
@@ -5,8 +5,129 @@ const TAC = props => {
     document.title = "AGB | Auction Template"
 
     return (
-        <StyledPage style={{ height: "100vh", backgroundColor: "#e6e6e6" }}>
-            <h1>Allgemeine Geschäftsbedingungen</h1>
+        <StyledPage style={{ minHeight: "100vh", backgroundColor: "#e6e6e6" }}>
+          <div style={{margin: "0 1em 0 1em"}}>
+            <h1>Allgemeine Geschäftsbedingungen - www.Auction-Template.com</h1>
+            <br />
+
+            <br />
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>§1 Geltung gegenüber Unternehmern und Begriffsdefinitionen</p>
+            (1) Die nachfolgenden Allgemeinen Geschäftbedingungen gelten für alle Lieferungen zwischen uns und einem Verbraucher
+            in ihrer zum Zeitpunkt der Bestellung gültigen Fassung.<br />
+            <br />
+            Verbraucher ist jede natürliche Person, die ein Rechtsgeschäft zu Zwecken abschließt, die überwiegend weder
+            ihrer gewerblichen noch ihrer selbständigen beruflichen Tätigkeit zugerechnet werden können (§ 13 BGB).
+            <br />
+
+            <br />
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>§2 Zustandekommen eines Vertrages, Speicherung des Vertragstextes</p>
+            (1) Die folgenden Regelungen über den Vertragsabschluss gelten für Bestellungen über unseren Internetshop https://auction-template.com .<br />
+            <br />
+            <b>(2) Im Falle des Vertragsschlusses kommt der Vertrag mit<br />
+
+              <div style={{fontWeight: "bold", marginLeft: "20px"}}>
+                <br />
+                Auction Template<br />
+                Inh. Joyce Marvin Rafflenbeul<br />
+                Hahler Straße 285<br />
+                D-32427 Minden<br />
+                <br />
+              </div>
+              zustande.<br />
+            </b>
+            <br />
+            (3) Die Präsentation der Waren in unserem Internetshop stellen kein rechtlich bindendes Vertragsangebot unsererseits dar,
+            sondern sind nur eine unverbindliche Aufforderungen an den Verbraucher, Waren zu bestellen. Mit der Bestellung der gewünschten Ware gibt der Verbraucher ein für
+            ihn verbindliches Angebot auf Abschluss eines Kaufvertrages ab.<br />
+            (4) Bei Eingang einer Bestellung in unserem Internetshop gelten folgende Regelungen:
+            Der Verbraucher gibt ein bindendes Vertragsangebot ab, indem er die in unserem Internetshop vorgesehene Bestellprozedur erfolgreich durchläuft.<br />
+            <br />
+            Die Bestellung erfolgt in folgenden Schritten:<br />
+            <br />
+            <strong>1) Auswahl der gewünschten Auktionsvorlage<br />
+
+            2) Anmeldung mit E-Mail und Passwort auf der Seite "Registrieren"<br />
+
+            3) Bestätigen durch Anklicken des PayPal "Direct Ceckout"-Buttons<br />
+
+            4) Verbindliche Absendung der Bestellung durch Anklicken des Buttons „Jetzt bezahlen“ im PayPal-Checkout-Prozess<br />
+
+            5) Anmeldung nach Registrierung und Eingabe der Anmeldeangaben (E-Mail-Adresse und Passwort). <br /></strong>
+            <br/>
+            Der Verbraucher kann vor dem verbindlichen Absenden der Bestellung durch Betätigen der in dem von ihm verwendeten Internet-Browser enthaltenen „Zurück“-Taste nach
+            Kontrolle seiner Angaben wieder zu der Internetseite gelangen, auf der die Angaben des Kunden erfasst werden und Eingabefehler berichtigen bzw. durch Schließen des
+            Internetbrowsers den Bestellvorgang abbrechen.
+            Wir bestätigen den Eingang der Bestellung unmittelbar durch eine automatisch generierte E-Mail („Auftragsbestätigung“). Mit dieser nehmen wir Ihr Angebot an.
+            <br />
+            <br />
+            (5) Speicherung des Vertragstextes bei Bestellungen über unseren Internetshop : Wir senden Ihnen die Bestelldaten und unsere AGB per
+            E-Mail zu. Die AGB können Sie jederzeit auch unter https://auction-template.com/TermsAndConditions einsehen.
+
+            Ihre Bestelldaten sind aus Sicherheitsgründen nicht mehr über das Internet zugänglich.
+            <br />
+
+
+            <br />
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>§3 Preise, Versandkosten, Zahlung, Fälligkeit</p>
+            (1) Die angegebenen Preise enthalten die gesetzliche Umsatzsteuer und sonstige Preisbestandteile. Hinzu kommen etwaige Versandkosten.<br />
+            <br />
+            (2) Der Verbraucher hat die Möglichkeit der Zahlung per
+            PayPal, SEPA-Lastschrift, GiroPay, Klarna Sofort & Kreditkarte. Alle Zahlungsarten werden über PayPal abgewickelt.<br />
+            <br />
+            (3) Differenzbesteuerung gem. § 19 USTG, ein Ausweis von Umsatzsteuer ist daher nicht möglich.
+
+            <br />
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>§4 Lieferung</p>
+            (1) Sofern wir dies in der Produktbeschreibung nicht deutlich anders angegeben haben, sind alle von uns angebotenen Artikel sofort versandfertig.
+            Die Lieferung erfolgt hier spätesten innerhalb von 1 Werktagen.
+            Dabei beginnt die Frist für die Lieferung im Falle der Zahlung per Vorkasse am Tag nach
+            Zahlungsauftrag an die mit der Überweisung beauftragte Bank und bei allen anderen Zahlungsarten am Tag nach Vertragsschluss zu laufen.
+            Fällt das Fristende auf einen Samstag, Sonntag oder gesetzlichen Feiertag am Lieferort, so endet die Frist am nächsten Werktag.
+            <br /><br />
+
+            (2) Die Gefahr des zufälligen Untergangs und der zufälligen Verschlechterung der
+            verkauften Sache geht auch beim Versendungskauf erst mit der Übergabe der Sache an den Käufer auf diesen über. <br />
+
+
+            <br />
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>§5 Eigentumsvorbehalt</p>
+            Sie erhalten bei Kauf einer unserer Auktionsvorlagen die Lizenz zur Nutzung unseres Angebots. Sollten Sie gegen unsere AGB verstoßen, behalten wir uns das Recht vor, Ihr Konto vorübergehend zu deaktivieren oder im Extremfall zu löschen. Einen Anspruch auf Erstattung bereits geleisteter Zahlungen entfällt an dieser Stelle.<br />
+            Die Weitergabe erworbener Auktionsvorlagen oder Ihrer Anmeldedaten an Dritte, sowie die übermäßige Generierung von kostenpflichtigen Auktionsvorlagen für diverse eBay-Konten ist strengstens untersagt und führt zu oben genannten Konsequenzen.
+            <br />
+
+            <br />
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>§6 Widerrufsrecht des Kunden als Verbraucher
+            </p>
+
+            <b>
+              Widerrufsrecht für Verbraucher<br /></b>
+            <br />
+            Verbrauchern steht ein Widerrufsrecht nach folgender Maßgabe zu, wobei Verbraucher jede natürliche Person ist,
+            die ein Rechtsgeschäft zu Zwecken abschließt, die überwiegend weder ihrer gewerblichen noch ihrer selbständigen
+            beruflichen Tätigkeit zugerechnet werden können:
+
+            <br />
+            <br />
+
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>Widerrufsbelehrung</p>
+            <br />
+            <b>Widerrufsrecht</b><br />
+            <br />
+            Sie erhalten auf unsere Angebote auf Grund der auschließlichen Verfügbarkeit per Download kein Widerrufsrecht. Weitere Informationen zur rechtlichen Grundlage erhalten Sie hier: <a href="https://www.gesetze-im-internet.de/bgb/__312g.html">§ 312d Abs. 4 BGB Nr. 1</a>
+            <br />
+            <br/>
+            Es gelten die gesetzlichen Gewährleistungsregelungen.
+            <br />
+
+
+            <br />
+            <p style={{fontWeight: "bold", paddingBottom: "10px"}}>§9 Vertragssprache</p>
+            Als Vertragssprache steht ausschließlich Deutsch zur Verfügung.<br />
+
+            <br />
+            <br />
+            <p>Stand der AGB Apr. 2020</p>
+          </div>
         </StyledPage>
     )
 }
